b6d287318c9be8fa3a8566925b27b57dd9d1a51b
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2026-01-03 01:34:10 +0400

    .

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: '/api',         
+  baseURL: 'https://api.mydressway.ge/api',         
   timeout: 5000,
   withCredentials: true,     
 });
